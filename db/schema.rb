0e1880d2286f953772ca0d907ce95388d4e29193
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-09-01 17:20:05 +0900

    Fix endusers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_061100) do
+ActiveRecord::Schema.define(version: 2020_09_01_081045) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 2020_09_01_061100) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "family_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "family_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.text "address", default: "", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
