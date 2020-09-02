db23b114068ea7f080f29777436a336936508020
vagrant <vagrant@localhost.localdomain>
2020-09-02 11:48:03 +0000

    Make public/items index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,20 @@
 class Public::ItemsController < Public::Base
+  def index
+    @genres = Genre.all
+    item_index
+  end
+
+  def show
+  end
+
+  private
+
+  def item_index
+    if params[:genre_id]
+      @items = Item.where(genre_id: params[:genre_id]).page(params[:page]).per(8)
+      @genre = Genre.find(params[:genre_id])
+    else
+      @items = Item.page(params[:page]).per(8)
+    end
+  end
 end
