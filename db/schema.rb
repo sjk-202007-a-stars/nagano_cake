e3364a789a7b7fd3970d95e8779f16fc39de1db0
vagrant <vagrant@localhost.localdomain>
2020-09-02 07:12:27 +0000

    Make admin/items function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_024445) do
+ActiveRecord::Schema.define(version: 2020_09_02_050657) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2020_09_02_024445) do
     t.text "introduction", null: false
     t.integer "price", null: false
     t.string "image_id"
-    t.boolean "sales_status", default: false, null: false
+    t.boolean "sales_status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["genre_id"], name: "index_items_on_genre_id"
