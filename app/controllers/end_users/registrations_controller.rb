56d13745ef6bec24c2779b859f6cfdd676b60119
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-09-03 10:50:33 +0900

    Add devise params

--- app/controllers/end_users/registrations_controller.rb
+++ app/controllers/end_users/registrations_controller.rb
@@ -41,9 +41,9 @@ class EndUsers::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :phone_number, :address, :email])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -59,4 +59,8 @@ class EndUsers::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end
