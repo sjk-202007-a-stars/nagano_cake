9c4acf34b836e32171d6e963c4f736ed2b5e479a
vagrant <vagrant@localhost.localdomain>
2020-09-03 05:40:48 +0000

    local_merge

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,1 +1,32 @@
 class Public::ShippingAddressesController < Public::Base
+  def index
+    @end_user = current_end_user
+    # @shipping_addresses = @end_user.shipping_address
+    @shipping_addresses = ShippingAddress.all
+    @shipping_address = ShippingAddress.new
+  end
+
+  def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    @shipping_address._id = current_end_user.id
+    @shipping_address.save
+    redirect_to shipping_addresses_path
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def shipping_address_params
+    params.require(:shipping_address).permit(:postal_code, :address, :name)
+  end
+end
