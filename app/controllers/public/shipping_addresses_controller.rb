2832fe9a7ecefdd122eb258594ec80612145147b
vagrant <vagrant@localhost.localdomain>
2020-09-03 09:54:02 +0000

    [Finish]shipping_addrres

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,28 +1,45 @@
 class Public::ShippingAddressesController < Public::Base
   def index
-    @end_user = current_end_user
-    # @shipping_addresses = @end_user.shipping_address
+    # @end_user = current_end_user
+    # @shipping_addresses = ShippingAddresses.find_by(current_end_user.id)
     @shipping_addresses = ShippingAddress.all
+    shipping_addresses = @shipping_addresses
+    @myshipping_addresses = current_end_user.shipping_addresses
     @shipping_address = ShippingAddress.new
   end
 
   def create
     @shipping_address = ShippingAddress.new(shipping_address_params)
-    @shipping_address._id = current_end_user.id
-    @shipping_address.save
-    redirect_to shipping_addresses_path
+    @shipping_address.end_user_id = current_end_user.id
+    if @shipping_address.save
+      redirect_to shipping_addresses_path
+    else
+    	@shipping_addresses = ShippingAddress.all
+      shipping_addresses = @shipping_addresses
+      @myshipping_addresses = current_end_user.shipping_addresses
+      @shipping_address = ShippingAddress.new
+      render :index
+    end
   end
 
   def edit
-
+    @shipping_address = ShippingAddress.find(params[:id])
   end
 
   def update
-
+  	shipping_address = ShippingAddress.find(params[:id])
+    if shipping_address.update(shipping_address_params)
+      redirect_to shipping_addresses_path
+    else
+    	@shipping_address = ShippingAddress.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
-
+  	shipping_address = ShippingAddress.find(params[:id])
+  	shipping_address.destroy
+  	redirect_to shipping_addresses_path
   end
 
   private
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,7 @@
 class ShippingAddress < ApplicationRecord
+  validates :postal_code, presence: true, length: { is: 7 }
+  validates :address, presence: true
+  validates :name, presence: true
+
   belongs_to :end_user
 end
