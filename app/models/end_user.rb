c3d2c831795ee9721042b043fe74c0dd8f81afb4
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-09-05 15:22:43 +0900

    Add end_user validation

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -3,4 +3,8 @@ class EndUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :family_name, :first_name, :postal_code, :address, :phone_number, presence: true
+  # 全角カナ入力のバリデーション
+  validates :family_name_kana, :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
 end
