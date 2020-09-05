dbd728560df01d38dfbd1bcd36da69fdb904b63f
vagrant <vagrant@localhost.localdomain>
2020-09-05 06:24:58 +0000

    Make public/cart_items index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,46 @@
 class Public::CartItemsController < Public::Base
+  def index
+    @cart_items = CartItem.where(end_user_id: current_end_user.id)
+    @total_price = 0
+  end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      flash[:alert] = "Please select a number"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    if cart_item.end_user_id == current_end_user.id
+      cart_item.destroy
+      redirect_to cart_items_path
+    else
+      render :index
+    end
+  end
+
+  def destroy_all
+    cart_items = CartItem.where(end_user_id: current_end_user.id)
+    cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :end_user_id, :quantity)
+  end
 end
