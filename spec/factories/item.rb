f20b3a2f727213b3123ee14ae9b049745f5d4988
vagrant <vagrant@localhost.localdomain>
2020-09-11 04:51:23 +0000

    Make user_registration_and_order_spec

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -7,4 +7,12 @@ FactoryBot.define do
     image_id { nil }
     sales_status { true }
   end
+  factory :item2, class: Item do
+    genre_id { 1 }
+    name { "チョコレートケーキ" }
+    introduction { "商品の説明です" }
+    price { 600 }
+    image_id { nil }
+    sales_status { true }
+  end
 end
---
+++ spec/factories/shipping_address.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :shipping_address do
+    end_user_id { 1 }
+    postal_code {　1000014 }
+    adddress { "東京都千代田区永田町１丁目７−１" }
+    name { "安倍晋三" }
+  end
+end
