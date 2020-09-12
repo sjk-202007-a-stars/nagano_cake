baf12b301106e2ca16380f5d01666bcb5c571a1b
vagrant <vagrant@localhost.localdomain>
2020-09-12 06:52:37 +0000

    [Fix]search

--- app/controllers/public/search_controller.rb
+++
@@ -1,7 +0,0 @@
-class Public::SearchController < Public::Base
-  def search
-    search = params[:search_method]
-    word = params[:search_method]
-    @items = Item.search(method, word)
-  end
-end
