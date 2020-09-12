97c2c699a7bcdfd19e249c187f79a7d736d63ad3
vagrant <vagrant@localhost.localdomain>
2020-09-12 03:41:09 +0000

    Finish rspec system test

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -10,12 +10,15 @@ class EndUser < ApplicationRecord
   validates :family_name, :first_name, :postal_code, :address, :phone_number, presence: true
   # 全角カナ入力のバリデーション
   validates :family_name_kana, :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-  
+
   has_many :cart_items
   has_many :shipping_addresses
 
   def active_for_authentication?
     super && (self.is_deleted == false)
+  end
+  def inactive_message
+    self.is_deleted? ? super : :You_have_been_withdrawn
   end
 
 end
