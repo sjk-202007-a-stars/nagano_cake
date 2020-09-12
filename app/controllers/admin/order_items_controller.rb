c153ae7c5d755c4e986565e6139b9578575f44a3
vagrant <vagrant@localhost.localdomain>
2020-09-12 03:13:15 +0000

    [Fix]status

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,13 +2,12 @@ class Admin::OrderItemsController < Admin::Base
 
   def update
     order_item = OrderItem.find(params[:id])
-    if order_item.update(order_item_params)
-        redirect_back(fallback_location: root_path)
+    order_item.update(order_item_params)
+      redirect_back(fallback_location: root_path)
   	end
-  end
 
   private
-   def order_item_params
-  	params.require(:order_item).permit(:making_status)
-   end
+  def order_item_params
+    params.require(:order_item).permit(:making_status)
+  end
 end
